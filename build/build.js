7a68aefc8cf234914db7a9c5d0cbbc1ff34aadf7
Josh Morrow <josh@jcmorrow.com>
2020-03-28 18:55:04 -0700

    Pretty happy with this imitation
    of https://i.redd.it/ttytu1g8izo41.png

--- build/build.js
+++ build/build.js
@@ -63,33 +63,46 @@ var Shapes = (function () {
     return Shapes;
 }());
 var CHANGE_RATE = 1.0 / 300.0;
-var FRAME_RATE = 30;
-var NOISE = 1.0 / 100.0;
-var PARTICLE_COUNT = 100;
-var SCALE = 20.0;
-var SPOKE_COUNT = 20;
+var FRAME_RATE = 60;
+var SPOKE_COUNT = 100;
+var NOISE = (Math.PI / SPOKE_COUNT) * 2.0;
+var DIAMETER = 15.0;
+var GRAVITY = 0.1;
 var time = 0;
 function setup() {
-    frameRate(30);
+    frameRate(FRAME_RATE);
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
 }
 function draw() {
     background(20, 20, 20);
-    translate(windowWidth / 2.0, windowHeight / 2.0);
+    translate(windowWidth / 2.0, windowHeight / 2.0 - 100.0);
+    rotate(Math.PI / 4.0);
     stroke(255, 255, 255);
-    for (var _i = 0, _a = _.range(SPOKE_COUNT); _i < _a.length; _i++) {
-        var theta = _a[_i];
-        for (var _b = 0, _c = _.range(PARTICLE_COUNT); _b < _c.length; _b++) {
-            var i = _c[_b];
-            var base = i * SCALE;
-            point(base +
-                0.5 *
-                    base *
-                    noise(Math.cos(theta) * 0.01, Math.sin(theta) * 0.01, time * NOISE), 0);
+    fill(20, 20, 20);
+    var points = _.range(SPOKE_COUNT).map(function (_) { return [0, 0]; });
+    var _loop_1 = function (outer) {
+        points = _.range(SPOKE_COUNT).map(function (i) {
+            var newDistance = [
+                coord(i, Math.cos) * noise(i * NOISE, outer * NOISE * 2.0) +
+                    outer * GRAVITY,
+                coord(i, Math.sin) * noise(i * NOISE, outer * NOISE * 2.0) +
+                    outer * GRAVITY
+            ];
+            return [points[i][0] + newDistance[0], points[i][1] + newDistance[1]];
+        });
+        for (var _i = 0, _a = _.range(SPOKE_COUNT); _i < _a.length; _i++) {
+            var i = _a[_i];
+            curve(points[i][0], points[i][1], points[(i + 1) % SPOKE_COUNT][0], points[(i + 1) % SPOKE_COUNT][1], points[(i + 2) % SPOKE_COUNT][0], points[(i + 2) % SPOKE_COUNT][1], points[(i + 3) % SPOKE_COUNT][0], points[(i + 3) % SPOKE_COUNT][1]);
         }
-        rotate((2.0 * Math.PI) / SPOKE_COUNT);
+    };
+    for (var _i = 0, _a = _.range(45); _i < _a.length; _i++) {
+        var outer = _a[_i];
+        _loop_1(outer);
     }
-    time += 1;
+}
+function coord(spoke, fn) {
+    var toFn = (spoke * Math.PI * 2.0) / SPOKE_COUNT;
+    return fn(toFn) * DIAMETER;
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,37 +1,56 @@
 const CHANGE_RATE = 1.0 / 300.0;
-const FRAME_RATE = 30;
-const NOISE = 1.0 / 100.0;
-const PARTICLE_COUNT = 100;
-const SCALE = 20.0;
-const SPOKE_COUNT = 20;
+const FRAME_RATE = 60;
+const SPOKE_COUNT = 100;
+const NOISE = (Math.PI / SPOKE_COUNT) * 2.0;
+const DIAMETER = 15.0;
+const GRAVITY = 0.1;
 
 let time = 0;
 
 function setup() {
-  frameRate(30);
+  frameRate(FRAME_RATE);
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 }
 
 function draw() {
   background(20, 20, 20);
-  translate(windowWidth / 2.0, windowHeight / 2.0);
+  translate(windowWidth / 2.0, windowHeight / 2.0 - 100.0);
+  rotate(Math.PI / 4.0);
 
   stroke(255, 255, 255);
+  fill(20, 20, 20);
 
-  for (let theta of _.range(SPOKE_COUNT)) {
-    for (let i of _.range(PARTICLE_COUNT)) {
-      let base = i * SCALE;
-      point(
-        base +
-          0.5 *
-            base *
-            noise(Math.cos(theta) * 0.01, Math.sin(theta) * 0.01, time * NOISE),
-        0
+  let points = _.range(SPOKE_COUNT).map(_ => [0, 0]);
+
+  for (let outer of _.range(45)) {
+    points = _.range(SPOKE_COUNT).map(i => {
+      let newDistance = [
+        coord(i, Math.cos) * noise(i * NOISE, outer * NOISE * 2.0) +
+          outer * GRAVITY,
+        coord(i, Math.sin) * noise(i * NOISE, outer * NOISE * 2.0) +
+          outer * GRAVITY
+      ];
+      return [points[i][0] + newDistance[0], points[i][1] + newDistance[1]];
+    });
+    for (let i of _.range(SPOKE_COUNT)) {
+      curve(
+        points[i][0],
+        points[i][1],
+        points[(i + 1) % SPOKE_COUNT][0],
+        points[(i + 1) % SPOKE_COUNT][1],
+        points[(i + 2) % SPOKE_COUNT][0],
+        points[(i + 2) % SPOKE_COUNT][1],
+        points[(i + 3) % SPOKE_COUNT][0],
+        points[(i + 3) % SPOKE_COUNT][1]
       );
     }
-    rotate((2.0 * Math.PI) / SPOKE_COUNT);
   }
 
-  time += 1;
+  // time += CHANGE_RATE;
+}
+
+function coord(spoke: any, fn: any) {
+  let toFn = (spoke * Math.PI * 2.0) / SPOKE_COUNT;
+  return fn(toFn) * DIAMETER;
 }
